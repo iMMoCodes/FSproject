d542c770743b97593e5c2289af744c0893ff4da0
iMMoCodes <matti-makela@hotmail.com>
2021-11-30 23:37:48 +0200

    Started project and created files
    -Created spring boot project boilerplate code
    -Added match data as csv
    -Created model for the match
    -Added spring batch and spring JPA to project

---
+++ ipldashboard/src/test/java/com/immocodes/ipldashboard/IpldashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immocodes.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
