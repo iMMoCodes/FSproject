d195e00857be3b89cdb67572c5ff9324750c6114
iMMoCodes <matti-makela@hotmail.com>
2021-12-01 22:53:27 +0200

    Added Router and links
    -Added react router
    -Created links to navigate between teams
    -Modified the fetching of the data to be dynamic

--- ipldashboard/src/frontend/src/pages/TeamPage.js
+++ ipldashboard/src/frontend/src/pages/TeamPage.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({ matches: [] });
-
-  const fetchMatches = async () => {
-    const response = await fetch('http://localhost:8080/team/Delhi%20Capitals');
-    const data = await response.json();
-    setTeam(data);
-  };
+  const { teamName } = useParams();
 
   useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
+      const data = await response.json();
+      setTeam(data);
+    };
     fetchMatches();
-  }, []);
+  }, [teamName]);
 
+  if (!team || !team.teamName) {
+    return <h1>Team not found</h1>;
+  }
   return (
     <div className='teamPage'>
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]} />
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
       {team.matches.slice(1).map((match) => (
-        <MatchSmallCard match={match} />
+        <MatchSmallCard teamName={team.teamName} match={match} />
       ))}
     </div>
   );
