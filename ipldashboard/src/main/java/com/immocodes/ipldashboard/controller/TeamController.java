fee0f4d51d444aebae0db986d8e146d7464857b5
iMMoCodes <matti-makela@hotmail.com>
2021-12-02 00:45:09 +0200

    Created endpoint to backend to get matches by year

--- ipldashboard/src/main/java/com/immocodes/ipldashboard/controller/TeamController.java
+++ ipldashboard/src/main/java/com/immocodes/ipldashboard/controller/TeamController.java
@@ -1,5 +1,9 @@
 package com.immocodes.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.immocodes.ipldashboard.model.Match;
 import com.immocodes.ipldashboard.model.Team;
 import com.immocodes.ipldashboard.repository.MatchRepository;
 import com.immocodes.ipldashboard.repository.TeamRepository;
@@ -7,6 +11,7 @@ import com.immocodes.ipldashboard.repository.TeamRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +32,15 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+                teamName,
+                startDate,
+                endDate);
     }
 }
