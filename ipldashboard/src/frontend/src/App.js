4e2e6c49576d6d612b3d5d7783d74bab9de2d97e
iMMoCodes <matti-makela@hotmail.com>
2021-12-02 20:48:45 +0200

    Created HomePage and All teams endpoint on backend

--- ipldashboard/src/frontend/src/App.js
+++ ipldashboard/src/frontend/src/App.js
@@ -2,17 +2,19 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Routes>
-          <Route path='/teams/:teamName' element={<TeamPage />}></Route>
+          <Route path='/teams/:teamName' element={<TeamPage />} />
           <Route
             path='/teams/:teamName/matches/:year'
             element={<MatchPage />}
-          ></Route>
+          />
+          <Route path='/' element={<HomePage />} />
         </Routes>
       </Router>
     </div>
