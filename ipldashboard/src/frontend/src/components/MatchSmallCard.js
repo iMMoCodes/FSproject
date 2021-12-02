2535115191b15405a8c0bb924bd2450d24a84b5a
iMMoCodes <matti-makela@hotmail.com>
2021-12-02 15:22:54 +0200

    Styled the Team page

--- ipldashboard/src/frontend/src/components/MatchSmallCard.js
+++ ipldashboard/src/frontend/src/components/MatchSmallCard.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './MatchSmallCard.scss';
+
 export const MatchSmallCard = ({ teamName, match }) => {
   if (!match) return null;
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div className='matchSmallCard'>
-      <h3>
-        vs <Link to={otherTeamRoute}>{otherTeam}</Link>
-      </h3>
-      <p>
+    <div
+      className={
+        isMatchWon ? 'matchSmallCard won-card' : 'matchSmallCard lost-card'
+      }
+    >
+      <span className='vs'>vs</span>
+      <h2>
+        <Link to={otherTeamRoute}>{otherTeam}</Link>
+      </h2>
+      <p className='match-result'>
         {match.matchWinner} won by {match.resultMargin} {match.result}
       </p>
     </div>
